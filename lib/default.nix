08fe25c6a4d6408ea64caea5f7147ad12b01dac0
zmrocze <karolochmanmilarski@gmail.com>
2024-06-17 22:43:04 +0200

    Add recursiveUpdateConcat

--- lib/default.nix
+++ lib/default.nix
@@ -6,10 +6,55 @@ lib: rec {
   # [a] -> (a -> AttrSet) -> AttrSet
   concatMapAttrSets = as: f: builtins.foldl' (set: a: set // f a) { } as;
 
-  concatFiles = lib.strings.concatMap builtins.readFile;
+  concatFiles = lib.strings.concatMapStrings builtins.readFile;
 
   # Recurse into the attrset and return all values that are not attrsets.
   # AttrSet -> [Any]
   flattenAttrset = atrs: builtins.concatMap (x: if builtins.isAttrs x then flattenAttrset x else [ x ]) (lib.attrsets.attrValues atrs);
+
+  #   ```
+  #   recursiveUpdateConcat :: AttrSet -> AttrSet -> AttrSet
+  #   ```
+  #   
+  #   Recursively merge attrsets but also concat lists.
+  # 
+  #   # Examples
+  #   :::{.example}
+  #   ## `lib.attrsets.recursiveUpdateConcat` usage example
+
+  #   ```nix
+  #   recursiveUpdateConcat {
+  #     boot.loader.grub.enable = true;
+  #     boot.loader.grub.devices = ["/dev/hda"];
+  #   } {
+  #     boot.loader.grub.devices = ["/dev/nvme"];
+  #   }
+
+  #   returns: {
+  #     boot.loader.grub.enable = true;
+  #     boot.loader.grub.devices = [ "/dev/hda" "/dev/nvme" ];
+  #   }
+  #   ```
+  recursiveUpdateConcat =
+    lhs:
+    rhs:
+    let
+      f = with builtins;
+        zipAttrsWith (n: values:
+          if length values == 1 then
+            head values
+          else
+            let
+              fst = head values;
+            in
+            if isAttrs fst then
+              f values # assuming all values are attrsets
+            else if isList fst then
+              concatLists values
+            else # return leftmost
+              head values
+        );
+    in
+    f [ rhs lhs ];
 
 }
